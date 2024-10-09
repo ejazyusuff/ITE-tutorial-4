bf50f6a055c5a2bbca23de6db97778f579b0685c
ejazyusuff <ejazyusuff42@gmail.com>
2024-10-09 17:12:53 -0400

    Add files via upload

---
+++ Hello world.py
@@ -0,0 +1,39 @@
+#ac = "      pops       "
+
+
+#ac = ac.strip()
+
+
+#message =(f"Programming Languages {ac}: \n\tJava \n\tC \n\tPython "  )
+#print(message)
+
+#name = "jAcK "
+#print(f"How are you {name}? ")
+
+#print(name.upper())
+#print(name.title())
+#print(name.lower())
+
+#famous_person = "Hamlet"
+#quote = "'To be or not to be'."
+#famous_quote =f"{famous_person} once said {quote}"
+
+#print(famous_quote)
+
+#name = "Timmy  "
+#print(name.rstrip())
+
+#print(3 + 5)
+#print(4 * 2)
+#print(16/2)
+#print(10 - 2)
+#print(2**3)
+
+#fav_num = 8
+#print(f"My favourite number is {fav_num}")
+#print("Hello world ")
+
+numbers = ["one ","two ","three "]
+print(numbers[-2].upper())
+
+
